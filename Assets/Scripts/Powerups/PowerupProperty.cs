a6cc5253322857cf29787a8a4a422d67940773d0
MEHDI\mshmw <mahdimashmooltm@gmail.com>
2025-07-28 10:26:26 +0330

    feat setup powerups SOs

---
+++ Assets/Scripts/Powerups/PowerupProperty.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Project.Powerups
+{
+    [CreateAssetMenu(fileName = "PowerupProperty", menuName = "Powerups/PowerupProperty")]
+    public class PowerupProperty : ScriptableObject
+    {
+        [field: SerializeField] public string PowerupName { private set; get; }
+        [field: SerializeField] public Sprite Icon { private set; get; }
+    }
+}
